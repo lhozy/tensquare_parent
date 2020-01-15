ea6f04d6322b91a0af5a159c977c9c55d3794522
Bruce.Lee <lhcn@qq.com>
2020-01-15 22:45:35 +0800

    edit spit

--- tensquare_spit/src/main/java/com/tensquare/spit/controller/SpitController.java
+++ tensquare_spit/src/main/java/com/tensquare/spit/controller/SpitController.java
@@ -2,9 +2,13 @@ package com.tensquare.spit.controller;
 
 import com.tensquare.spit.pojo.Spit;
 import com.tensquare.spit.service.SpitService;
+import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,7 +17,21 @@ import org.springframework.web.bind.annotation.*;
 public class SpitController {
     @Autowired
     private SpitService spitService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
+    /**
+     * 根据上级ID查询吐槽分页数据
+     * @param parentid
+     * @param page
+     * @param size
+     * @return
+     */
+    @GetMapping("/comment/{parentid}/{page}/{size}")
+    public Result listPageByParentid(@PathVariable String parentid , @PathVariable int page, @PathVariable int size){
+        Page<Spit> pageResponse = spitService.findSpitListPageByParentid(parentid, page, size);
+        return  new Result(true,StatusCode.OK,"查询成功",  new PageResult<Spit>(pageResponse.getTotalElements(), pageResponse.getContent()) );
+    }
     @GetMapping("/{id}")
     public Result findById(@PathVariable("id") String id){
         return new Result(true,StatusCode.OK,"查询成功",spitService.findById(id));
@@ -32,5 +50,15 @@ public class SpitController {
         spit.setId(id);
         spitService.update(spit);
         return new Result(true,StatusCode.OK,"修改成功");
+    }
+    @PutMapping("/thumbup/{id}")
+    public Result thumbup(@PathVariable("id") String id){
+        String userid = "111";
+        if (redisTemplate.opsForValue().get("thumbup" + userid) != null) {
+            return new Result(false,StatusCode.REPERROR,"不能重复点赞");
+        }
+        spitService.thumbup(id);
+        redisTemplate.opsForValue().set("thumbup"+userid,1);
+        return new Result(true,StatusCode.OK,"点赞成功");
     }
 }
--- tensquare_spit/src/main/java/com/tensquare/spit/service/SpitService.java
+++ tensquare_spit/src/main/java/com/tensquare/spit/service/SpitService.java
@@ -3,10 +3,17 @@ package com.tensquare.spit.service;
 import com.tensquare.spit.dao.SpitDao;
 import com.tensquare.spit.pojo.Spit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import util.IdWorker;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -17,41 +24,81 @@ public class SpitService {
     private SpitDao spitDao;
     @Autowired
     private IdWorker idWorker;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    /**
+     * 根据上级ID查询吐槽列表
+     *
+     * @param parentid
+     * @param page
+     * @param size
+     * @return
+     */
+    public Page<Spit> findSpitListPageByParentid(String parentid, int page, int size) {
+        return spitDao.findByParentid(parentid, PageRequest.of(page - 1, size));
+    }
 
     /**
      * 根据id查询
+     *
      * @param id
      * @return
      */
-    public Spit findById(String id){
+    public Spit findById(String id) {
         return spitDao.findById(id).get();
     }
 
     /**
      * 查询所有
+     *
      * @return
      */
-    public List<Spit> findAll(){
+    public List<Spit> findAll() {
         return spitDao.findAll();
     }
 
     /**
      * 新增
+     *
      * @param spit
      */
-    public void save(Spit spit){
-        spit.setId(idWorker.nextId()+"");
+    public void save(Spit spit) {
+        spit.setId(idWorker.nextId() + "");
+        spit.setPublishtime(new Date());
+        spit.setVisits(0);
+        spit.setShare(0);
+        spit.setThumbup(0);
+        spit.setComment(0);
+        spit.setState("1");
+        if (spit.getParentid() != null && "" != spit.getParentid()) {
+            //父节点的回复数+1
+            /*Query query = new Query();
+            query.addCriteria(Criteria.where("_id").is(spit.getParentid()));
+            Update update = new Update();
+            update.inc("comment",1);
+            mongoTemplate.updateFirst(query,update,"spit");*/
+            mongoTemplate.updateFirst(Query.query(Criteria.where("_id").is(spit.getParentid())), new Update().inc("comment", 1), "spit");
+        }
         spitDao.save(spit);
     }
 
     /**
      * 更新
+     *
      * @param spit
      */
-    public void update(Spit spit){
+    public void update(Spit spit) {
         spitDao.save(spit);
     }
-    public void delete(String id){
+
+    public void delete(String id) {
         spitDao.deleteById(id);
+    }
+
+    public void thumbup(String id) {
+        Spit spit = spitDao.findById(id).get();
+        spit.setThumbup(spit.getThumbup() + 1);
+        spitDao.save(spit);
     }
 }
